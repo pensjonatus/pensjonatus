ecf1373eb9bc6905e63cf3cbaf01c8a1141bf155
Paweł Kowaluk <pensjonatus@gmail.com>
2024-02-04 12:12:53 +0100

    simplify

--- src/scripts/menu.ts
+++
@@ -1,7 +0,0 @@
-document.querySelector('.hamburger')?.addEventListener('click', () => {
-  const navLinks = document.querySelector('.nav-links');
-  if (!navLinks) {
-    throw new Error('Cannot find nav links');
-  }
-  navLinks.classList.toggle('expanded');
-});
