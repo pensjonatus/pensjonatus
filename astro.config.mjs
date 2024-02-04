735eedf5ed942538fe3059d970e60cb7986ee4ae
Paweł Kowaluk <pensjonatus@gmail.com>
2024-02-04 13:34:40 +0100

    fix build error and favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pensjonatus.github.io',
-  base: '/pensjonatus'
+  base: '/pensjonatus',
   integrations: [preact()],
 });
