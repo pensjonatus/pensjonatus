e0f4773a271a3858c1709db4f5e91d18e881589a
Paweł Kowaluk <pensjonatus@gmail.com>
2024-02-10 09:53:00 +0100

    add cover image

--- src/components/LastRead.tsx
+++ src/components/LastRead.tsx
@@ -58,10 +58,31 @@ export function LastRead() {
   }
 
   return (
-    <div>
-      You recently read "{post.title}".{' '}
-      <a href={`/posts/${post.slug}/`}>Continue reading</a>
-    </div>
+    <a
+      href={`/posts/${post.slug}/`}
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '0.5rem',
+        border: '0.5px solid var(--text-color)',
+        borderRadius: 'var(--box-border-radius)',
+        padding: '1rem',
+        textDecoration: 'none',
+        lineHeight: 1,
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24"
+        viewBox="0 -960 960 960"
+        width="24"
+        fill="currentColor"
+      >
+        <path d="M320-160h320v-120q0-66-47-113t-113-47q-66 0-113 47t-47 113v120ZM160-80v-80h80v-120q0-61 28.5-114.5T348-480q-51-32-79.5-85.5T240-680v-120h-80v-80h640v80h-80v120q0 61-28.5 114.5T612-480q51 32 79.5 85.5T720-280v120h80v80H160Z" />
+      </svg>
+      <div>Continue reading "{post.title}"</div>
+    </a>
   );
 }
 
