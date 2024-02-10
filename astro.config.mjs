1a6a901554bd71c054de583d1b4a6c71118e44b0
Paweł Kowaluk <pensjonatus@gmail.com>
2024-02-10 14:46:22 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import sitemap from "@astrojs/sitemap";
 import pagefind from 'astro-pagefind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sighandfiction.com',
   build: {
-    format: 'file',
+    format: 'file'
   },
-  integrations: [preact(), pagefind()],
+  integrations: [preact(), pagefind(), sitemap()]
 });
