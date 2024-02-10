cbd3b8b791526c3dcfffcd7ffa4f68ad51456acb
Paweł Kowaluk <pensjonatus@gmail.com>
2024-02-10 14:42:29 +0100

    add search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import preact from '@astrojs/preact';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sighandfiction.com',
-  integrations: [preact()],
+  build: {
+    format: 'file',
+  },
+  integrations: [preact(), pagefind()],
 });
